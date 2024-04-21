6b18733cb78fc1ceceb0a9418ac919b3646ccceb
jhosefin <90260920+jhosefin@users.noreply.github.com>
2024-04-21 00:06:52 -0400

    componente login realizado

---
+++ front-reto/src/app/components/login/login.component.ts
@@ -0,0 +1,36 @@
+import {Component} from '@angular/core';
+import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
+import {FormControl, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {merge} from 'rxjs';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [MatFormFieldModule, MatInputModule, FormsModule, ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', [Validators.required, Validators.maxLength(8)]);
+  errorMessage = '';
+
+  constructor() {
+    merge(this.email.statusChanges, this.email.valueChanges)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.updateErrorMessage());
+  }
+
+  updateErrorMessage() {
+    if (this.email.hasError('required') || this.password.hasError('required')) {
+      this.errorMessage = '¡Completa los datos!';
+    } else if (this.email.hasError('email') || this.password.hasError('maxLength(8)')) {
+      this.errorMessage = '¡Datos  incorrectos!';
+    } else {
+      this.errorMessage = '';
+    }
+  }
+
+}
